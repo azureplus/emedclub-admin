95781f945e0d93741001b43490a1b31cdad215ab
larksoft <liweihai@huawei.com>
2017-09-12 09:04:12 +0800

    更新 Index.vue

--- src/components/medicine/Index.vue
+++ src/components/medicine/Index.vue
@@ -1,53 +1,94 @@
 <template>
-    <layout>
+    <layout :progressing="refreshing" :toast="toast">
+        <mu-content-block>
+            <mu-raised-button label="增加" @click="onAdd"/>
+
+            <mu-table ref="table" :showCheckbox="false" @rowClick="onView">
+                <mu-thead>
+                    <mu-tr>
+                        <mu-th>企业</mu-th>
+						<mu-th>名称</mu-th>
+                        <mu-th>分类</mu-th>
+						<mu-th>靶向</mu-th>
+						<mu-th>状态</mu-th>
+                    </mu-tr>
+                </mu-thead>
+                <mu-tbody>
+                    <mu-tr v-for="medicine in medicines" :key="medicine.id">
+                        <mu-td>{{merchant.merchant}}</mu-td>
+                        <mu-td>{{merchant.name}}</mu-td>
+						<mu-td>{{merchant.category}}</mu-td>
+						<mu-td>{{merchant.target}}</mu-td>
+						<mu-td>{{merchant.stateName}}</mu-td>
+                    </mu-tr>
+                </mu-tbody>
+            </mu-table>
+            
+            <mu-pagination :total="total" :current="current" @pageChange="onPage"></mu-pagination>
+        </mu-content-block>
     </layout>
 </template>
 
 <script>
-	import Layout from '../Layout'
-	import api from '../../api'
-	import { mapGetters, mapActions } from 'vuex'
-	import Mixin from '../../mixin'
+    import Layout from '../Layout'
+    import api from '../../api'
+    import { mapGetters, mapActions } from 'vuex'
+    import Mixin from '../../mixin'
 
-	export default {
-		mixins: [Mixin],
+    export default {
+        mixins: [Mixin],
 
         data () {
             return {
-                contracts: [],
+                medicines: [],
+                total: 0,
+                current: 1,
             }
         },
 
-		methods: {
-			onInitialize() {
-				this.onRefresh()
-			},
+        methods: {
+            onInitialize() {
+                var self = this;
 
-            onRefresh: function() {
-            	this.loadData()
+                api.count("Medicine").then(function(total){
+                    self.total = total
+                    self.current = 1
+                    self.wait(self.loadData())
+                })
             },
 
-		    loadData() {
-		    	var self = this;
+            onUpdate() {
+                this.onInitialize()
+            },
 
-				if (self.$route.query.merchant_id) {
-					var conditions = {merchant_id: self.$route.query.merchant_id};
-				} else if (self.$route.query.activity_id) {
-					var conditions = {activity_id: self.$route.query.activity_id};
-				} else {
-					var conditions = {customer_id: self.me.id};
-				}
-				
-                self.wait(api.query("Contract", conditions, {order: "update_time DESC"})).then(function(contracts) {
-                    self.contracts = contracts;
-                },function(err){
-                    reject(err);
-                });
-		    },
-		},
+            onRefresh() {
+                this.wait(this.loadData())
+            },
 
-    	components: {
-      		'layout': Layout,
-    	}
-	}
+            onAdd: function(index) {
+                this.goto("/medicine/new")
+            },
+
+            onView: function(index) {
+                this.goto("/medicine/view/" + this.medicines[index].id)
+            },
+
+            onPage(page) {
+                this.current = page;
+                this.wait(this.loadData())
+            },
+
+            loadData: function() {
+                var self = this;
+
+                return api.query("Medicine", {}, {limit: 20, offset: 20 * (self.current - 1)}).then(function(medicines){
+                    self.medicines = medicines
+                })
+            }
+        },
+
+        components: {
+            'layout': Layout,
+        }
+    }
 </script>
