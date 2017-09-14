4720bc2e6596325bebe6b151c50c3b69f216d895
larksoft <liweihai@huawei.com>
2017-09-14 11:26:41 +0800

    更新 Information.js

--- src/model/Information.js
+++ src/model/Information.js
@@ -23,7 +23,15 @@ export default class Information extends Model {
 
     static createDefault() {
         return new Information({
-            category: 0, author: '', title: '', media: '', content: ''
+            category: 0, author: '', title: '', media: '', content: '', total_views:0, total_comments: 0, total_likes: 0, price: 0, state: 0,
         })
+    }
+
+    get categories() {
+        return ['课堂', '会议', '人才', '融资', '公司']
+    }
+
+    get categoryName() {
+        return this.categories[this.category]
     }
 }
